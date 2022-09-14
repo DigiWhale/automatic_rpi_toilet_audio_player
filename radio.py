0d53e619c7fb57efa6f5aa9766422434c802bb3c
digiwhale <digiwhale.humphrey@gmail.com>
2022-09-14 12:57:18 -0400

    1

---
+++ radio.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+import pygame
+import alsaaudio
+
+# m = alsaaudio.Mixer()
+# current_volume = m.getvolume() # Get the current Volume
+# m.setvolume(100) # Set the volume to 70%.
+
+switch = 31
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+while True:
+  if GPIO.input(switch):
+    pygame.mixer.init()
+    pygame.mixer.music.load("/home/pi/Sailor.mp3")
+    pygame.mixer.music.play()
+    pygame.mixer.music.pause()
+    while True:
+      if GPIO.input(switch):
+        pygame.mixer.music.unpause()
+      else:
+        pygame.mixer.music.pause()
+
