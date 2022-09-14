c950c839330228e6ebf2eece30d23ddac3d4d5ad
digiwhale <digiwhale.humphrey@gmail.com>
2022-09-14 13:45:56 -0400

    1

--- get_latest_video.py
+++ get_latest_video.py
@@ -6,5 +6,32 @@ load_dotenv()
 
 api = Api(api_key=os.getenv("YOUTUBE_API_KEY"))
 
-channel_by_id = api.get_channel_info(for_username="CNN")
-print(channel_by_id.items[0].to_dict())
+def get_videos(channel_id):
+    api = pyyoutube.Api(api_key=API_KEY)
+    channel_info = api.get_channel_info(channel_id=channel_id)
+
+    playlist_id = channel_info.items[0].contentDetails.relatedPlaylists.uploads
+
+    uploads_playlist_items = api.get_playlist_items(
+        playlist_id=playlist_id, count=10, limit=6
+    )
+
+    videos = []
+    for item in uploads_playlist_items.items:
+        video_id = item.contentDetails.videoId
+        video = api.get_video_by_id(video_id=video_id)
+        videos.extend(video.items)
+    return videos
+
+
+def processor():
+    channel_id = "UC_x5XG1OV2P6uZZ5FSM9Ttw"
+    videos = get_videos(channel_id)
+
+    with open("videos.json", "w+") as f:
+        for video in videos:
+            f.write(video.to_json())
+            f.write("\n")
+            
+if __name__ == "__main__":
+    processor()
