258cf831e66e25629a84c69b5657b23bec6b4c4a
digiwhale <digiwhale.humphrey@gmail.com>
2022-09-14 13:56:37 -0400

    1

--- get_latest_video.py
+++ get_latest_video.py
@@ -1,10 +1,23 @@
 from pyyoutube import Api
 import os
 from dotenv import load_dotenv
+from youtube_dl import YoutubeDL
 
 load_dotenv()
-
+link = "https://www.youtube.com/watch?v="
 api = Api(api_key=os.getenv("YOUTUBE_API_KEY"))
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': '/home/pi/latest.mp3',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+
+audio_downloader = YoutubeDL(ydl_opts)
 
 def get_videos(channel_id):
     channel_info = api.get_channel_info(for_username=channel_id)
@@ -26,12 +39,8 @@ def get_videos(channel_id):
 def processor():
     channel_id = "CNN"
     videos = get_videos(channel_id)
-    print(videos[0].snippet.publishedAt, videos[0].snippet.title, videos[0].id)
-
-    with open("videos.json", "w+") as f:
-        for video in videos:
-            f.write(video.to_json())
-            f.write("\n")
+    # print(videos[0].snippet.publishedAt, videos[0].snippet.title, videos[0].id)
+    audio_downloader.download([link + videos[0].id])
 
 if __name__ == "__main__":
     processor()
