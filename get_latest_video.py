d3e07a43bced38cc7411f71f040593d431e5abbe
digiwhale <digiwhale.humphrey@gmail.com>
2022-09-14 13:35:44 -0400

    1

--- get_latest_video.py
+++ get_latest_video.py
@@ -6,5 +6,5 @@ load_dotenv()
 
 api = Api(api_key=os.getenv("YOUTUBE_API_KEY"))
 
-channel_by_id = api.get_channel_info(channel_id="UC_x5XG1OV2P6uZZ5FSM9Ttw")
+channel_by_id = api.get_channel_info(for_username="CNN")
 print(channel_by_id.items)
