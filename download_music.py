cbd2aec2087b7a4cffc29edb80ed6d0436918c79
digiwhale <digiwhale.humphrey@gmail.com>
2022-09-14 14:00:03 -0400

    1

--- download_music.py
+++ download_music.py
@@ -3,7 +3,7 @@ from youtube_dl import YoutubeDL
 link = "https://www.youtube.com/watch?v=J3Gy2Mikr1s"
 ydl_opts = {
     'format': 'bestaudio/best',
-    'outtmpl': '/home/pi/latest.mp3',
+    'outtmpl': '/home/pi/latest.%(ext)s',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
