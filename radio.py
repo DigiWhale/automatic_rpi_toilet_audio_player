fb498afdd3548fbd299e02d05cb1e9f401e51f54
digiwhale <digiwhale.humphrey@gmail.com>
2022-09-20 13:54:37 -0400

    1

--- radio.py
+++ radio.py
@@ -16,14 +16,18 @@ while True:
   if GPIO.input(switch):
     pygame.mixer.init()
     pygame.mixer.music.load("/home/pi/latest_video.mp3")
+    print("Loading latest video")
     pygame.mixer.music.play()
     pygame.mixer.music.pause()
     while pygame.mixer.music.get_busy() == True:
       if GPIO.input(switch):
         pygame.mixer.music.unpause()
+        print("Unpaused")
       else:
         pygame.mixer.music.pause()
+        print("Paused")
     try:
+      print("Getting new video")
       processor()
     except:
       pass
