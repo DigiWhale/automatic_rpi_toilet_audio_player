13a55e444845c1a5b55ec7f933206760d182f678
digiwhale <digiwhale.humphrey@gmail.com>
2022-09-20 14:55:35 -0400

    1

--- get_latest_video.py
+++ get_latest_video.py
@@ -37,7 +37,7 @@ def get_videos(channel_id):
 
 
 def processor():
-    channel_id = "FOX"
+    channel_id = "CNN"
     videos = get_videos(channel_id)
     # print(videos[0].snippet.publishedAt, videos[0].snippet.title, videos[0].id)
     audio_downloader.download([link + videos[0].id])
