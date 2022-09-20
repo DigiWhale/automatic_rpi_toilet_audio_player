f4450920de6478decc9f6f93632b8d12c2d321db
digiwhale <digiwhale.humphrey@gmail.com>
2022-09-20 13:56:30 -0400

    1

--- radio.py
+++ radio.py
@@ -12,20 +12,23 @@ switch = 31
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+paused = False
 while True:
   if GPIO.input(switch):
+    processor()
     pygame.mixer.init()
     pygame.mixer.music.load("/home/pi/latest_video.mp3")
     print("Loading latest video")
     pygame.mixer.music.play()
     pygame.mixer.music.pause()
+    paused = True
     while pygame.mixer.music.get_busy() == True:
       if GPIO.input(switch):
         pygame.mixer.music.unpause()
-        print("Unpaused")
+        # print("Unpaused")
       else:
         pygame.mixer.music.pause()
-        print("Paused")
+        # print("Paused")
     try:
       print("Getting new video")
       processor()
